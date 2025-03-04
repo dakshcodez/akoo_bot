8faa288f451da4640119ff6e4a0e090bf58e1293
dakshcodez <daksharoracodez@gmail.com>
2025-03-05 00:15:49 +0530

    added gemini api

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from discord.ext import commands
 import asyncio
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
-DISCORD_TOKEN = os.getenv('TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GEMINI_TOKEN = os.getenv('GEMINI_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix = '!', intents=intents)
+client = genai.Client(api_key = GEMINI_TOKEN)
 
 @bot.event
 async def on_ready():
@@ -49,5 +52,12 @@ async def remind(interaction: discord.Interaction, time: int, unit: str, message
    ## await interaction.response.send_message(f"Reminder set for {time} seconds!", ephemeral=True)
     await asyncio.sleep(n_time)
     await interaction.followup.send(f"⏰{interaction.user.mention} Reminder: {message}", ephemeral=True)
+
+@bot.tree.command(name="ai", description="Ask gemini 2.0 flash")
+async def ai(interaction: discord.Interaction, message: str):
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents=list(message))
+    await interaction.response.send_message(f"{response.text}")
 
 bot.run(DISCORD_TOKEN)
